23166dbe46424130b491f2c47861c57d880c4d38
kento.yamashita <kento.yamashita@eure.jp>
2016-01-05 09:42:14 +0000

    modify check-cert.go

--- check-cert/README.md
+++ check-cert/README.md
@@ -4,15 +4,9 @@
 
 Check for days on the expiration date of the SSL certificate of DNS-NAME.
 
-## Usage
-
-```shell
-check-cert [-host=<host>] [-crit=<crit-days>] [-warn=<warn-days>]
-```
-
 ## Setting
 
 ```
 [plugin.checks.ssl-cert]
-command = "/path/to/check-cert -host=example.com -crit=15 -warn=30"
+command = "/path/to/check-cert -d example.com -c 15 -w 30"
 ```
--- check-cert/check-cert.go
+++ check-cert/check-cert.go
@@ -15,11 +15,11 @@ var timeNow = time.Now()
 
 var opts struct {
 	Domain           string `short:"d" long:"domain" required:"true" description:"monitor domain name"`
-	WarningDayCount  int    `short:"w" long:"warning" default:"15" description:"warning if once past the day"`
-	CriticalDayCount int    `short:"c" long:"critical" default:"30" description:"critical if once past the day"`
+	WarningDayCount  int    `short:"w" long:"warning" default:"30" description:"warning if once past the day"`
+	CriticalDayCount int    `short:"c" long:"critical" default:"15" description:"critical if once past the day"`
 }
 
-func GetCert(host string) ([]*x509.Certificate, error) {
+func getCert(host string) ([]*x509.Certificate, error) {
 	conn, err := tls.Dial("tcp", host+":443", nil)
 	var peerCertificates []*x509.Certificate
 	if err == nil {
@@ -38,7 +38,7 @@ func run(args []string) *checkers.Checker {
 	result := checkers.OK
 	msg := ""
 
-	peerCertificates, err := GetCert(opts.Domain)
+	peerCertificates, err := getCert(opts.Domain)
 	if err != nil {
 		return checkers.Unknown(err.Error())
 	}
@@ -47,17 +47,18 @@ func run(args []string) *checkers.Checker {
 		duration := certNotAfter.Sub(timeNow)
 		diffDays := int(duration.Hours() / 24)
 		if diffDays < opts.CriticalDayCount {
-			msg = fmt.Sprintf("CRITICAL: It is another %d days on the expiration date of the SSL certificate of %s.",
+			msg = fmt.Sprintf("It is another %d days on the expiration date of the SSL certificate of %s.",
 				diffDays, opts.Domain)
 			result = checkers.CRITICAL
 			break
 		} else if diffDays < opts.WarningDayCount {
-			msg = fmt.Sprintf("WARNING: It is another %d days on the expiration date of the SSL certificate of %s.",
+			msg = fmt.Sprintf("It is another %d days on the expiration date of the SSL certificate of %s.",
 				diffDays, opts.Domain)
 			result = checkers.WARNING
 			break
-		} else {
-			msg = fmt.Sprintf("SUCCESS: It is another %d days on the expiration date of the SSL certificate of %s.",
+		}
+		if msg == "" {
+			msg = fmt.Sprintf("It is another %d days on the expiration date of the SSL certificate of %s.",
 				diffDays, opts.Domain)
 		}
 	}
